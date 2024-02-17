0104f0025a174c144002b5c8de3c8f863aac56f6
Pritam Saha <pritamsaha0995@gmail.com>
2024-02-17 19:23:29 +0530

    Remove unnecessary methods

--- src/main/java/com/piggybank/piggybank/repository/WealthRepository.java
+++ src/main/java/com/piggybank/piggybank/repository/WealthRepository.java
@@ -1,9 +1,10 @@
 package com.piggybank.piggybank.repository;
 
+import com.piggybank.piggybank.entity.WealthEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WealthRepository extends JpaRepository<WealthRepository, Long> {
+public interface WealthRepository extends JpaRepository<WealthEntity, Long> {
 
 }
